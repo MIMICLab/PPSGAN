335207fafa23a7bca3e21a13cbe9cfb7e9219f87
datamining107 <jamesk1228@gmail.com>
2019-09-02 19:47:22 +0900

    modify pre-training method

--- image_model/train.py
+++ image_model/train.py
@@ -15,8 +15,7 @@ z_dim = int(sys.argv[3])
 prev_iter = int(sys.argv[4])
 NUM_CLASSES = 10
 USE_DELTA = True
-USE_ADAMW = True
-USE_WGAN_GP = False
+
 
 mb_size, X_dim, width, height, channels,len_x_train, x_train, y_train, len_x_test, x_test, y_test  = data_loader(dataset)
 
@@ -68,48 +67,57 @@ with graph.as_default():
                                                                    Z_S,
                                                                    USE_DELTA)  
 
-        if USE_WGAN_GP:
-            D_real_logits = discriminator_gp(X, var_D)
-            D_fake_logits = discriminator_gp(G_sample, var_D)
-            C_real_logits = classifier_gp(X, var_C)
-            C_fake_logits = classifier_gp(G_sample, var_C)
-             
-            gp = gradient_penalty(G_sample, X, mb_size, var_D)
-            D_S_loss = tf.reduce_mean(D_fake_logits) - tf.reduce_mean(D_real_logits) + 10.0*gp 
-            D_C_loss = tf.reduce_mean(
-                       tf.nn.softmax_cross_entropy_with_logits_v2(labels = Y, 
-                                                                  logits = C_real_logits))        
-            G_zero_loss = tf.reduce_mean(tf.pow(X - G_zero,2))         
-            G_S_loss = - tf.reduce_mean(D_fake_logits)
-            G_C_loss = tf.reduce_mean(
-                       tf.nn.softmax_cross_entropy_with_logits_v2(labels = Y, 
-                                                                  logits = C_fake_logits))
-            
-        else:
-            D_real, D_real_logits = discriminator(X, var_D)
-            D_fake, D_fake_logits = discriminator(G_sample, var_D)
-            C_real_logits = classifier(X, var_C)
-            C_fake_logits = classifier(G_sample, var_C)            
-            D_real_loss = tf.nn.sigmoid_cross_entropy_with_logits(
+        D_real, D_real_logits = discriminator(X, var_D)
+        D_fake, D_fake_logits = discriminator(G_sample, var_D)
+        C_real_logits = classifier(X, var_C)
+        C_fake_logits = classifier(G_sample, var_C)            
+        D_real_loss = tf.nn.sigmoid_cross_entropy_with_logits(
                             logits=D_real_logits, labels=tf.ones_like(D_real))
-            D_fake_loss = tf.nn.sigmoid_cross_entropy_with_logits(
+        D_fake_loss = tf.nn.sigmoid_cross_entropy_with_logits(
                                logits=D_fake_logits, labels=tf.zeros_like(D_fake))
             
-            D_S_loss = tf.reduce_mean(D_real_loss) + tf.reduce_mean(D_fake_loss)
-            D_C_loss = tf.reduce_mean(
+        D_S_loss = tf.reduce_mean(D_real_loss) + tf.reduce_mean(D_fake_loss)
+        D_C_loss = tf.reduce_mean(
                        tf.nn.softmax_cross_entropy_with_logits_v2(labels = Y, 
                                                                   logits = C_real_logits))                   
-            G_zero_loss = tf.reduce_mean(tf.pow(X - G_zero,2))         
-            G_S_loss = tf.reduce_mean(
+        G_zero_loss = tf.reduce_mean(tf.pow(X - G_zero,2))         
+        G_S_loss = tf.reduce_mean(
                        tf.nn.sigmoid_cross_entropy_with_logits(logits=D_fake_logits, 
                                                                labels=tf.ones_like(D_fake)))
-            G_C_loss = tf.reduce_mean(
+        G_C_loss = tf.reduce_mean(
                        tf.nn.softmax_cross_entropy_with_logits_v2(labels = Y, 
                                                                   logits = C_fake_logits))
 
-        
         D_loss = D_S_loss + D_C_loss
         G_loss = G_S_loss + G_C_loss + G_zero_loss
+        
+        #pre-train losses
+        D_real_pre, D_real_logits_pre = discriminator(X, var_D)
+        D_fake_pre, D_fake_logits_pre = discriminator(G_zero, var_D)
+        C_real_logits_pre = classifier(X, var_C)
+        C_fake_logits_pre = classifier(G_zero, var_C)   
+        
+        D_real_loss_pre = tf.nn.sigmoid_cross_entropy_with_logits(
+                            logits=D_real_logits_pre, labels=tf.ones_like(D_real_pre))
+        D_fake_loss_pre = tf.nn.sigmoid_cross_entropy_with_logits(
+                               logits=D_fake_logits_pre, labels=tf.zeros_like(D_fake_pre))
+            
+        D_S_loss_pre = tf.reduce_mean(D_real_loss_pre) + tf.reduce_mean(D_fake_loss_pre)
+        D_C_loss_pre = tf.reduce_mean(
+                       tf.nn.softmax_cross_entropy_with_logits_v2(labels = Y, 
+                                                                  logits = C_real_logits_pre))                           
+        G_S_loss_pre = tf.reduce_mean(
+                       tf.nn.sigmoid_cross_entropy_with_logits(logits=D_fake_logits_pre, 
+                                                               labels=tf.ones_like(D_fake_pre)))
+        G_C_loss_pre = tf.reduce_mean(
+                       tf.nn.softmax_cross_entropy_with_logits_v2(labels = Y, 
+                                                                  logits = C_fake_logits_pre))
+        
+        D_loss_pre = D_S_loss_pre + D_C_loss_pre
+        G_loss_pre = G_S_loss_pre + G_C_loss_pre + G_zero_loss
+
+        
+        #sensitivity estimation
         latent_max = tf.reduce_max(z_original, axis = 0)
         latent_min = tf.reduce_min(z_original, axis = 0)          
         
@@ -133,15 +141,12 @@ with graph.as_default():
 
         num_batches_per_epoch = int((len_x_train-1)/mb_size) + 1
         
-        if USE_ADAMW:
-            A_solver = tf.contrib.opt.AdamWOptimizer(weight_decay=1e-4,learning_rate=1e-4,beta1=0.5, beta2=0.9).minimize(G_zero_loss,var_list=var_G, global_step=global_step)       
-            D_solver = tf.contrib.opt.AdamWOptimizer(weight_decay=1e-4,learning_rate=1e-4,beta1=0.5, beta2=0.9).minimize(D_loss,var_list=var_D_C, global_step=global_step)
-            G_solver = tf.contrib.opt.AdamWOptimizer(weight_decay=1e-4,learning_rate=1e-4,beta1=0.5, beta2=0.9).minimize(G_loss,var_list=var_G, global_step=global_step)
+
+        D_solver_pre = tf.contrib.opt.AdamWOptimizer(weight_decay=1e-4,learning_rate=1e-4,beta1=0.5, beta2=0.9).minimize(D_loss_pre,var_list=var_D_C, global_step=global_step) 
+        G_solver_pre = tf.contrib.opt.AdamWOptimizer(weight_decay=1e-4,learning_rate=1e-4,beta1=0.5, beta2=0.9).minimize(G_loss_pre,var_list=var_G, global_step=global_step) 
+        D_solver = tf.contrib.opt.AdamWOptimizer(weight_decay=1e-4,learning_rate=1e-4,beta1=0.5, beta2=0.9).minimize(D_loss,var_list=var_D_C, global_step=global_step)
+        G_solver = tf.contrib.opt.AdamWOptimizer(weight_decay=1e-4,learning_rate=1e-4,beta1=0.5, beta2=0.9).minimize(G_loss,var_list=var_G, global_step=global_step)
         
-        else:
-            A_solver = tf.train.AdamOptimizer(learning_rate=1e-4,beta1=0.5, beta2=0.9).minimize(G_zero_loss,var_list=var_G, global_step=global_step)       
-            D_solver = tf.train.AdamOptimizer(learning_rate=1e-4,beta1=0.5, beta2=0.9).minimize(D_loss,var_list=var_D_C, global_step=global_step)
-            G_solver = tf.train.AdamOptimizer(learning_rate=1e-4,beta1=0.5, beta2=0.9).minimize(G_loss,var_list=var_G, global_step=global_step)
 
         timestamp = str(int(time.time()))
         if not os.path.exists('results/'):
@@ -179,20 +184,40 @@ with graph.as_default():
                 if USE_DELTA:
                     enc_noise = np.random.normal(0.0,1.0,[mb_size,z_dim]).astype(np.float32)  
                 else:
-                    enc_noise = np.random.laplace(0.0,1.0,[mb_size,z_dim]).astype(np.float32)  
-                summary,_, A_loss_curr= sess.run([merged, A_solver, G_zero_loss],
-                                                        feed_dict={X: X_mb, 
+                    enc_noise = np.random.laplace(0.0,1.0,[mb_size,z_dim]).astype(np.float32)
+                
+                _, D_curr, D_S_curr, D_C_curr = sess.run([D_solver_pre, 
+                                                          D_loss_pre, D_S_loss_pre, D_C_loss_pre],
+                                                         feed_dict={X: X_mb, 
                                                                    Y: Y_mb, 
                                                                    Z_noise: enc_zero, 
                                                                    Z_S: enc_zero}) 
+
+                summary, _, G_curr, G_S_curr, G_C_curr, G_z_curr = sess.run([merged, 
+                                                                             G_solver_pre,
+                                                                             G_loss_pre,
+                                                                             G_S_loss_pre, 
+                                                                             G_C_loss_pre,
+                                                                             G_zero_loss],
+                                                        feed_dict={X: X_mb, 
+                                                                   Y: Y_mb,  
+                                                                   Z_noise: enc_zero,
+                                                                   Z_S: enc_zero}) 
+            current_step = tf.train.global_step(sess, global_step)
+            train_writer.add_summary(summary,current_step)
+        
+            if idx % 100 == 0:
+                print('Iter: {}; D_loss: {:.4}; D_S: {:.4}; D_C: {:.4}; '.format(idx,
+                                                                                  D_curr, D_S_curr,
+                                                                                  D_C_curr))
+                print('Iter: {}; G_loss: {:.4}; G_S: {:.4}; G_C: {:.4}; G_zero: {:.4};'.format(idx, G_curr, G_S_curr, G_C_curr, G_z_curr))
                 
                 current_step = tf.train.global_step(sess, global_step)
                 train_writer.add_summary(summary,current_step)
-                if idx % 100 == 0:
-                    print('Iter: {}; G_zero_loss: {:.4};'.format(idx,A_loss_curr))
-                if idx % 1000 == 0: 
-                    path = saver.save(sess, checkpoint_prefix, global_step=current_step)
-                    print('Saved model at {} at step {}'.format(path, current_step)) 
+
+            if idx % 1000 == 0: 
+                path = saver.save(sess, checkpoint_prefix, global_step=current_step)
+                print('Saved model at {} at step {}'.format(path, current_step)) 
                     
         #calculate approximated global sensitivity            
         for idx in range(num_batches_per_epoch):
