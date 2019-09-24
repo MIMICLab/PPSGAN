4928083014b2b639301ecc02629e49be05d18c23
datamining107 <jamesk1228@gmail.com>
2019-09-24 23:26:31 +0900

    minor change

--- image_model/classifier_eval.py
+++ image_model/classifier_eval.py
@@ -53,7 +53,7 @@ def classifier_one(dataset, model_name, x_target, y_target, len_x_target):
 
             num_batches_per_epoch = int((len_x_target-1)/mb_size) + 1
 
-            C_solver = tf.contrib.opt.AdamWOptimizer(weight_decay=1e-4,learning_rate=1e-4,beta1=0.5, beta2=0.9).minimize(C_loss,var_list=var_C, global_step=global_step)
+            C_solver = tf.contrib.opt.AdamWOptimizer(weight_decay=1e-4,learning_rate=1e-3,beta1=0.5, beta2=0.9).minimize(C_loss,var_list=var_C, global_step=global_step)
 
 
             timestamp = str(int(time.time()))    
@@ -62,7 +62,7 @@ def classifier_one(dataset, model_name, x_target, y_target, len_x_target):
 
             x_temp = np.append(x_test, x_test[:mb_size], axis=0)
             y_temp = np.append(y_test, y_test[:mb_size], axis=0)
-            for it in range(num_batches_per_epoch*400):
+            for it in range(num_batches_per_epoch*200):
                 X_mb, Y_mb = next_batch(mb_size, x_target, y_target)
                 _, C_curr = sess.run([C_solver, C_loss], feed_dict={X: X_mb, Y: Y_mb})
                 if it % 100 == 0:
